de055b45cbdba68c2b9944e65330bcea71b46bfb
sreyapsuresh <sreyapsuresh@gmail.com>
2024-03-20 22:24:50 -0500

    Initial Commit

---
+++ GrammarQuiz/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GrammarQuiz
+//
+//  Created by Sreya Suresh on 3/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GrammarQuiz/GrammarQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrammarQuizApp.swift
+//  GrammarQuiz
+//
+//  Created by Sreya Suresh on 3/20/24.
+//
+
+import SwiftUI
+
+@main
+struct GrammarQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
